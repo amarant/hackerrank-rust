59999958f2823926c4fca015c2112ed9b300c326
Arnaud Marant <arnaudmarant@gmail.com>
2015-08-19 22:22:50 +0200

    remove old slow solution for stock maximize

--- src/algorithms/dynamic_programming/stock_maximize.rs
+++ src/algorithms/dynamic_programming/stock_maximize.rs
@@ -1,8 +1,5 @@
 use std::io;
 use std::io::BufRead;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry::{Occupied, Vacant};
-use std::fmt::{Debug, Formatter, Error};
 
 fn stock_maximize(stock_values: &Vec<i64>) -> i64 {
     let mut money = 0i64;
@@ -15,56 +12,6 @@ fn stock_maximize(stock_values: &Vec<i64>) -> i64 {
         }
     }
     money
-}
-
-#[derive(Hash, Clone)]
-struct StockPosition {
-    stock_count: i32,
-    money: i32,
-}
-impl Debug for StockPosition {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(f, "[stocks:{} money:{}]",
-            self.stock_count, self.money)
-    }
-}
-
-fn stock_maximize2(stock_values: &Vec<i32>) -> i32 {
-    let mut stock_positions :HashMap<i32, StockPosition> = HashMap::new();
-    stock_positions.insert(0, StockPosition {stock_count: 0, money: 0});
-    for stock_value in stock_values.iter() {
-        let mut new_stock_position :HashMap<i32, StockPosition> = HashMap::new();
-        for position in stock_positions.values() {
-            let position_do_nothing = position.clone();
-            insert_if_more_money(&mut new_stock_position, position_do_nothing);
-
-            let mut position_buy = position.clone();
-            position_buy.stock_count += 1;
-            position_buy.money -= *stock_value;
-            insert_if_more_money(&mut new_stock_position, position_buy);
-
-            let mut position_sell = position.clone();
-            position_sell.money += position.stock_count * stock_value;
-            position_sell.stock_count = 0;
-            insert_if_more_money(&mut new_stock_position, position_sell);
-        }
-        //println!("stock value:{} positions:{:?}", stock_value, new_stock_position);
-        stock_positions = new_stock_position;
-    }
-    stock_positions.values().map(|pos| pos.money).max().unwrap()
-}
-
-fn insert_if_more_money(
-    stock_positions: &mut HashMap<i32, StockPosition>,
-    position: StockPosition) {
-    match stock_positions.entry(position.stock_count) {
-        Vacant(entry) => {entry.insert(position); ()},
-        Occupied(mut entry) => {
-            if entry.get().money < position.money {
-                entry.insert(position);
-            }
-        },
-    }
 }
 
 #[allow(dead_code)]
